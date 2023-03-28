9794e17f2c00b5c8e673d781671078f642877f37
astolyarova3 <126551029+astolyarova3@users.noreply.github.com>
2023-03-28 10:27:22 +0300

    Add files via upload

---
+++ lab4/src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	Circle circle = new Circle(10);
+        System.out.println(circle.getSquare());
+    }//вывели площадь круга
+}
